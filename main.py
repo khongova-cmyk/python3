d7d0b3b8302f57d1c750631cf2a7ec4a3c51d8f1
khongova-cmyk <khongova@mail.ru>
2025-11-27 04:16:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,4 @@
-def get_number():
-    while True:
-        try:
-            num = int(input("Введите число от 1 до 5: "))
-            if 1 <= num <= 5:
-                return num
-            else:
-                print("Число должно быть от 1 до 5. Попробуйте снова.")
-        except ValueError:
-            print("Ошибка! Пожалуйста, введите целое число.")
-
-
-number_words = {
-    1: "one",
-    2: "two",
-    3: "three",
-    4: "four",
-    5: "five"
-}
-
-number = get_number()
-
-print(f"Число {number} на английском: {number_words[number]}")
+get_number = int(input("Enter a number: "))
+while get_number >= 0:
+    print(get_number)
+    get_number -= 1
