b82e57786decb20e1fee6b2e35a8453417ca7ab0
khongova-cmyk <khongova@mail.ru>
2025-11-29 14:19:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-get_number = int(input("Enter a number: "))
+while True:
+    try:
+        get_number = int(input("Enter a number: "))
+        if get_number < 0:
+            raise ValueError("Число не должно быть отрицательным!")
+        break
+    except ValueError as e:
+        print(f"Ошибка: {e}. Попробуйте снова.")
+
 while get_number >= 0:
     print(get_number)
     get_number -= 1
